45e71e7b154da00bbaef45c2438e0ceb6aa22742
jackmarley254 <engndiritu1@gmail.com>
2023-10-09 21:27:46 +0300

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(s):
 	for char in s:
-	if ord('a') <= ord(char) <= 0rd('z'):
-		print(chr(ord(char) - 32), end="")
-
-		else:
-		print(char, end="")
-		print("\n", end="")
+		if ord('a') <= ord(char) <= ord('z'):
+			print(chr(ord(char) - 32), end="")
+			print(char, end="")
+			print("\n", end="")
