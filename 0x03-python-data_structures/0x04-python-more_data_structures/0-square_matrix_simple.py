24cbd889152404ff77888fb7844eee790bafd9f1
jackmarley254 <engndiritu1@gmail.com>
2023-10-12 05:45:45 +0300

    Squared simple

--- 0x03-python-data_structures/0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-data_structures/0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
