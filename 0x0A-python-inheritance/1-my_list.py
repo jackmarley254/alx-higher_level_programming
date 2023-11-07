a0f83717215bc0e989aac16078f5b1b9f0a05dbf
jackmarley254 <engndiritu1@gmail.com>
2023-11-07 04:46:19 +0300

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains My_list class.
+"""
+
+class Mylist(list):
+	""" subclass of list """
+	def __int__(self):
+		""" initialises the object"""
+		super().__init__()
+
+	def print_sorted list(self):
+		""" prints the sorted list"""
+		print(sorted(seff))
