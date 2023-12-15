30d6ffe9ffd61001ab24dc86ef4e7277af22c3cd
jackmarley254 <engndiritu1@gmail.com>
2023-12-16 01:50:01 +0300

    Read user/My privileges debugging

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2.
---CREATE USER
-   -- IF NOT EXITS 'user_0d_1', 'user_0d_2'@'localhost';
---GRANT ALL PRIVILEGES
-  --  ON *.*
-    --TO 'user_0d_1','user_0d_2'@'localhost';
---FLUSH PRIVILEGES;
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of the users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
