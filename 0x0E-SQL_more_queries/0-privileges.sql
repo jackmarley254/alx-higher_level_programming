c92a66328c9b6285f3328859f93eb9cd41413fe8
jackmarley254 <engndiritu1@gmail.com>
2023-12-15 22:39:05 +0300

    My privileges.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -4,3 +4,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
