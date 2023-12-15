a4c451b7abe31bf386d0dd258549558af431309f
jackmarley254 <engndiritu1@gmail.com>
2023-12-15 22:30:43 +0300

    My privileges.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2.
-
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
+CREATE USER 'user_0d_1', 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
